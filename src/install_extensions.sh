54dea98ad60be6311477bbecc8cd72bb76d344a8
Akhram Shaik <akhramshaik@gmail.com>
2024-10-07 09:57:24 +0530

    Update install_extensions.sh

--- src/install_extensions.sh
+++ src/install_extensions.sh
@@ -10,26 +10,26 @@ pip install -r custom_nodes/ComfyUI-Manager/requirements.txt
 #2 - ComfyUI Workspace Manager
 git clone https://github.com/11cafe/comfyui-workspace-manager.git custom_nodes/comfyui-workspace-manager
 
-#controlnet_aux
+#3 - ControlNet Auxiliary Preprocessors
 git clone https://github.com/Fannovel16/comfyui_controlnet_aux custom_nodes/comfyui_controlnet_aux
 pip install -r custom_nodes/comfyui_controlnet_aux/requirements.txt
 
-#IPADAPTER
+#4 - ComfyUI IPAdapter Plus
 git clone https://github.com/cubiq/ComfyUI_IPAdapter_plus custom_nodes/ComfyUI_IPAdapter_plus
+
+#5 - WAS Node Suite
+git clone https://github.com/WASasquatch/was-node-suite-comfyui custom_nodes/was-node-suite-comfyui
+pip install -r custom_nodes/was-node-suite-comfyui/requirements.txt
+
+#6 - WAS Extra Nodes
+git clone https://github.com/WASasquatch/WAS_Extras custom_nodes/WAS_Extras
+python custom_nodes/WAS_Extras/install.py
 
 # reactor
 git clone https://github.com/Gourieff/comfyui-reactor-node custom_nodes/comfyui-reactor-node
 python custom_nodes/comfyui-reactor-node/install.py
 wget -P ~/models/facerestore_models https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth
 wget -P ~/models/facerestore_models https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/GFPGANv1.4.pth
-
-# WAS node nodes
-git clone https://github.com/WASasquatch/was-node-suite-comfyui custom_nodes/was-node-suite-comfyui
-pip install -r custom_nodes/was-node-suite-comfyui/requirements.txt
-
-# WAS extra nodes
-git clone https://github.com/WASasquatch/WAS_Extras custom_nodes/WAS_Extras
-python custom_nodes/WAS_Extras/install.py
 
 #sdxl_prompt_styler
 git clone https://github.com/twri/sdxl_prompt_styler custom_nodes/sdxl_prompt_styler
