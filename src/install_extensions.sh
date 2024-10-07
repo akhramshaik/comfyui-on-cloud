b9985748b85de1b8982136ca029e76ef1b89431d
Akhram Shaik <akhramshaik@gmail.com>
2024-10-07 17:21:26 +0530

    Update install_extensions.sh

--- src/install_extensions.sh
+++ src/install_extensions.sh
@@ -11,19 +11,18 @@ pip install -r custom_nodes/ComfyUI-Manager/requirements.txt
 git clone https://github.com/11cafe/comfyui-workspace-manager.git custom_nodes/comfyui-workspace-manager
 
 #3 - ControlNet Auxiliary Preprocessors
-git clone https://github.com/Fannovel16/comfyui_controlnet_aux custom_nodes/comfyui_controlnet_aux
+git clone https://github.com/Fannovel16/comfyui_controlnet_aux.git custom_nodes/comfyui_controlnet_aux
 pip install -r custom_nodes/comfyui_controlnet_aux/requirements.txt
 
 #4 - ComfyUI IPAdapter Plus
-git clone https://github.com/cubiq/ComfyUI_IPAdapter_plus custom_nodes/ComfyUI_IPAdapter_plus
+git clone https://github.com/cubiq/ComfyUI_IPAdapter_plus.git custom_nodes/ComfyUI_IPAdapter_plus
 
 #5 - WAS Node Suite
-git clone https://github.com/WASasquatch/was-node-suite-comfyui custom_nodes/was-node-suite-comfyui
+git clone https://github.com/WASasquatch/was-node-suite-comfyui.git custom_nodes/was-node-suite-comfyui
 pip install -r custom_nodes/was-node-suite-comfyui/requirements.txt
 
 #6 - WAS Extra Nodes
-git clone https://github.com/WASasquatch/WAS_Extras custom_nodes/WAS_Extras
-python custom_nodes/WAS_Extras/install.py
+git clone https://github.com/WASasquatch/WAS_Extras.git custom_nodes/WAS_Extras
 
 #7 - ComfyUI To Python Code
 git clone https://github.com/pydn/ComfyUI-to-Python-Extension.git custom_nodes/ComfyUI-to-Python-Extension
@@ -37,13 +36,13 @@ pip install -r custom_nodes/ComfyUI-WD14-Tagger/requirements.txt
 git clone https://github.com/pythongosssss/ComfyUI-Custom-Scripts.git custom_nodes/ComfyUI-Custom-Scripts
 
 #10 - ReActorFaceSwap
-git clone https://github.com/Gourieff/comfyui-reactor-node custom_nodes/comfyui-reactor-node
+git clone https://github.com/Gourieff/comfyui-reactor-node.git custom_nodes/comfyui-reactor-node
 python custom_nodes/comfyui-reactor-node/install.py
 wget -P ~/models/facerestore_models https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth
 wget -P ~/models/facerestore_models https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/GFPGANv1.4.pth
 
 #11 - Impact-Pack
-git clone https://github.com/ltdrdata/ComfyUI-Impact-Pack custom_nodes/ComfyUI-Impact-Pack
+git clone https://github.com/ltdrdata/ComfyUI-Impact-Pack.git custom_nodes/ComfyUI-Impact-Pack
 python custom_nodes/ComfyUI-Impact-Pack/install.py
 
 #12 - UltimateSDUpscale
