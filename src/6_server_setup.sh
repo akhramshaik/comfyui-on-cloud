f62949cbe3d1871e11d6d3fcdd4cb2e7f211e3ea
Akhram Shaik <akhramshaik@gmail.com>
2024-10-07 13:18:21 +0530

    Update 6_server_setup.sh

--- src/6_server_setup.sh
+++ src/6_server_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Current directory: $(pwd)"
+
 comfy_ui_dir="$(pwd)/ComfyUI"
 echo "ComfyUI Directory: $comfy_ui_dir"
 
 #Dynamically creating run_the_server.sh
-# Create the run_the_server.sh file
 echo "#!/bin/bash" > run_the_server.sh
 echo "" >> run_the_server.sh
 echo "python main.py --listen" >> run_the_server.sh
@@ -17,31 +17,3 @@ cp run_the_server.sh ComfyUI
 echo " "
 echo " ---------------- custom run_the_server.sh created "
 echo " "
-
-
-
-SERVICE_FILE_CONTENT="[Unit]
-Description=ComfyUI Server
-
-[Service]
-Type=simple
-ExecStart=${comfy_ui_dir}/run_the_server.sh
-
-
-
-[Install]
-WantedBy=multi-user.target"
-
-# Create the systemd service file
-echo "$SERVICE_FILE_CONTENT" | sudo tee /etc/systemd/system/comfyui.service > /dev/null
-
-# Reload systemd to recognize the new service
-sudo systemctl daemon-reload
-sudo systemctl enable comfyui.service
-sudo systemctl start comfyui.service
-
-echo " "
-echo " ---------------- setup completed "
-echo " "
-echo -e " ---------------- For health check go to this address $(curl -s httpbin.org/ip | jq -r .origin):8188 in your browser"
-
