0b35ce78f1c7f8fad74d157e84d0d33d881c751c
Akhram Shaik <akhramshaik@gmail.com>
2024-10-07 13:14:24 +0530

    Update 6_server_setup.sh

--- src/6_server_setup.sh
+++ src/6_server_setup.sh
@@ -1,42 +1,14 @@
 #!/bin/bash
 
-USER_NAME=$(whoami)
-USER_HOME=$(eval echo ~$USER_NAME)
-
-
 echo "Current directory: $(pwd)"
-script_dir=$(dirname "$(realpath "$0")")
-echo "script_dir : $script_dir"
-parent_dir=$(dirname "$script_dir")
-echo "parent_dir Directory: $parent_dir"
-parentparent_dir=$(dirname "$parent_dir")
-echo "parentparent_dir Directory: $parentparent_dir"
-comfy_ui_dir="$parentparent_dir/ComfyUI"
+comfy_ui_dir="$(pwd)/ComfyUI"
 echo "ComfyUI Directory: $comfy_ui_dir"
 
-PYTHON_PATH="${USER_HOME}/miniconda3/bin/python"
-BASHRC_PATH="${USER_HOME}/.bashrc"
-COMFYUI_RUNNER_PATH="${comfy_ui_dir}/main.py"
-COMFYUI_PATH=$comfy_ui_dir
-
-
-
-# Dynamically creating run_the_server.sh
-
+#Dynamically creating run_the_server.sh
 # Create the run_the_server.sh file
 echo "#!/bin/bash" > run_the_server.sh
 echo "" >> run_the_server.sh
-echo "BASHRC_PATH=\"$BASHRC_PATH\"" >> run_the_server.sh
-echo "PYTHON_PATH=\"$PYTHON_PATH\"" >> run_the_server.sh
-echo "COMFYUI_RUNNER_PATH=\"${COMFYUI_PATH}/main.py\"" >> run_the_server.sh
-echo "" >> run_the_server.sh
-echo "# Source the bashrc file" >> run_the_server.sh
-echo "source \$BASHRC_PATH" >> run_the_server.sh
-echo "" >> run_the_server.sh
-echo "# Start ComfyUI using the defined paths" >> run_the_server.sh
-echo "if ! sudo -u $USER_NAME \$PYTHON_PATH \$COMFYUI_RUNNER_PATH --listen; then" >> run_the_server.sh
-echo "    echo \"Error: Failed to start ComfyUI\" >&2" >> run_the_server.sh
-echo "fi" >> run_the_server.sh
+echo "python main.py --listen" >> run_the_server.sh
 
 # Make the script executable
 chmod +x run_the_server.sh
